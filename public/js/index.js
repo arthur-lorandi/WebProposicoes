6d68529b9f0a488189455e705796aa8f80604685
arthur-lorandi <arthurlorandi05@gmail.com>
2025-04-16 19:39:54 -0300

    .gitignore changes & package configurations

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 const addPropButton = document.getElementById("addPropButton");
 
 addPropButton.addEventListener("click", () => {
+    //--------testing button--------
     document.getElementById("p1").append("13");
+    //--------testing button--------
     const truthTable = document.getElementById("truthTable");
     const prop = document.getElementById("propTextInput");
     
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const http = require("http");
 const express = require('express');
 const favicon = require('serve-favicon');
-// const fs = require('fs');
-// const fsp = fs.promises;
 const path = require('path');
-// const url = require('url');
 
 const app = express();
 const host = 'localhost';
